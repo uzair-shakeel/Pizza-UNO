b5ae00abad8c84aafa1d40146b648682c6aab8a2
vikram kumar <vikramkumar8t8@gmail.com>
2024-05-15 22:24:02 +0500

    'update-page'

--- client/src/pages/CookiePolicy.jsx
+++ client/src/pages/CookiePolicy.jsx
@@ -4,23 +4,37 @@ const CookiePolicy = () => {
   return (
     <div style={{ margin: '50px auto', width: '100%', maxWidth: '1100px' }}>
     <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
-        <h3 style={{ fontSize: '15px' }}>This Privacy Policy describes how your personal information is collected, used, and shared when you visit or make a purchase from this website (the "Site").</h3>
-        <h3 style={{ fontSize: '15px' }}>PERSONAL INFORMATION WE COLLECT</h3>
-        <h3 style={{ fontSize: '15px' }}>When you visit the Site, we automatically collect certain information about your device, including information about your web browser, IP address, time zone, and some of the cookies that are installed on your device. Additionally, as you browse the Site, we collect information about the individual web pages or products that you view, what websites or search terms referred you to the Site, and information about how you interact with the Site. We refer to this automatically-collected information as "Device Information."</h3>
-        <h3 style={{ fontSize: '15px' }}>We collect Device Information using the following technologies:</h3>
-        <h3 style={{ fontSize: '15px' }}>    - "Cookies"are data files that are placed on your device or computer and often include an anonymous unique identifier. For more information about cookies, and how to disable cookies,</h3>
-        <h3 style={{ fontSize: '15px' }}>                visit http://www.allaboutcookies.org.</h3>
-        <h3 style={{ fontSize: '15px' }}>    - "Log files" track actions occurring on the Site, and collect data including your IP address, browser type, Internet service provider, referring/exit pages, and date/time stamps.</h3>
-        <h3 style={{ fontSize: '15px' }}>    - "Web beacons","tags","pixels are electronic files used to record information about how you browse the Site.</h3>
-        <h3 style={{ fontSize: '15px' }}>Additionally when you make a purchase or attempt to make a purchase through the Site, we collect certain information from you, including your name, billing address, shipping address, payment information (including credit card numbers), email address, and phone number.  We refer to this information as "Order Information."</h3>
-        <h3 style={{ fontSize: '15px' }}>When we talk about "Personal Information" in this Privacy Policy, we are talking both about Device Information and Order Information.</h3>
-        <h3 style={{ fontSize: '15px' }}>HOW DO WE USE YOUR PERSONAL INFORMATION?</h3>
-        <h3 style={{ fontSize: '15px' }}>We use the Order Information that we collect generally to fulfill any orders placed through the Site (including processing your payment information, arranging for delivery, and providing you with invoices and/or order confirmations).  Additionally, we use this Order Information to:</h3>
-        <h3 style={{ fontSize: '15px' }}>Communicate with you in particular changes to store opening times and status of your order via email, phone or SMS ;</h3>
-        <h3 style={{ fontSize: '15px' }}>Screen our orders for potential risk or fraud; and</h3>
-        <h3 style={{ fontSize: '15px' }}>When in line with the preferences you have shared with us, provide you with information or advertising relating to our products or services.</h3>
-        <h3 style={{ fontSize: '15px' }}>We use the Device Information that we collect to help us screen for potential risk and fraud (in particular, your IP address), and more generally to improve and optimize our Site (for example, by generating analytics about how our customers browse and interact with the Site, and to assess the success of our marketing and advertising campaigns).</h3>
-        <h3 style={{ fontSize: '15px' }}>We use the Device Information that we collect to show your targeted advertising on our products,services or promotions.</h3>
+        <h3 style={{ fontSize: '15px' }}>About cookies</h3>
+        <h3 style={{ fontSize: '15px' }}>This website uses cookies.  By using this website and agreeing to this policy, you consent to Pizza Uno Sunderland use of cookies in accordance with the terms of this policy.</h3>
+
+        <h3 style={{ fontSize: '15px' }}>Cookies are files sent by web servers to web browsers, and stored by the web browsers.</h3>
+
+        <h3 style={{ fontSize: '15px' }}>The information is then sent back to the server each time the browser requests a page from the server.  This enables a web server to identify and track web browsers.</h3>
+
+        <h3 style={{ fontSize: '15px' }}>There are two main kinds of cookies: session cookies and persistent cookies.  Session cookies are deleted from your computer when you close your browser, whereas persistent cookies remain stored on your computer until deleted, or until they reach their expiry date.</h3>
+
+        <h3 style={{ fontSize: '15px' }}>Cookies on our website</h3>
+
+        <h3 style={{ fontSize: '15px' }}>We use a cookie to recognise your computer when you visit our website, in order to populate the login box and record whether you are logged in or not. We only do this if you have explicitly registered with us.</h3>
+
+        <h3 style={{ fontSize: '15px' }}> We also use a cookie to tell us that you have agreed to this Cookies Policy, if you have chosen to do so.</h3>
+
+        <h3 style={{ fontSize: '15px' }}>A cookie in no way gives us access to your computer or any information about you, however we are committed to ensuring that your information is secure. In order to prevent unauthorised access or disclosure we have put in place suitable physical, electronic and managerial procedures to safeguard and secure the information we collect online.</h3>
+
+        <h3 style={{ fontSize: '15px' }}>Pizza Uno Sunderland uses the following cookies on this website, for the following purposes:</h3>
+
+        <h3 style={{ fontSize: '15px' }}>Google cookies</h3>
+
+        <h3 style={{ fontSize: '15px' }}>Pizza Uno Sunderland uses Google Analytics to analyse the use of this website.  Google Analytics generates statistical and other information about website use by means of cookies, which are stored on users' computers.  The information generated relating to our website is used to create reports about the use of the website. Google will store and use this information. Google's privacy policy is available at: http://www.google.com/privacypolicy.html</h3>
+
+        <h3 style={{ fontSize: '15px' }}>Refusing cookies</h3>
+
+        <h3 style={{ fontSize: '15px' }}>Most browsers allow you to refuse or accept cookies.</h3>
+        <h3 style={{ fontSize: '15px' }}>In Internet Explorer, you can refuse all cookies by clicking “Tools”, “Internet Options”, “Privacy”, and selecting “Block all cookies” using the sliding selector.</h3>
+
+        <h3 style={{ fontSize: '15px' }}>In Firefox, you can adjust your cookies settings by clicking “Tools”, “Options” and “Privacy”.</h3>
+
+        <h3 style={{ fontSize: '15px' }}>Blocking cookies will have a negative impact upon the usability of some websites.</h3>
     </div>
 </div>
   )
--- client/src/pages/ServiceDisclaimer.jsx
+++ client/src/pages/ServiceDisclaimer.jsx
@@ -4,23 +4,39 @@ const ServiceDisclaimer = () => {
   return (
     <div style={{ margin: '50px auto', width: '100%', maxWidth: '1100px' }}>
     <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
-        <h3 style={{ fontSize: '15px' }}>This Privacy Policy describes how your personal information is collected, used, and shared when you visit or make a purchase from this website (the "Site").</h3>
-        <h3 style={{ fontSize: '15px' }}>PERSONAL INFORMATION WE COLLECT</h3>
-        <h3 style={{ fontSize: '15px' }}>When you visit the Site, we automatically collect certain information about your device, including information about your web browser, IP address, time zone, and some of the cookies that are installed on your device. Additionally, as you browse the Site, we collect information about the individual web pages or products that you view, what websites or search terms referred you to the Site, and information about how you interact with the Site. We refer to this automatically-collected information as "Device Information."</h3>
-        <h3 style={{ fontSize: '15px' }}>We collect Device Information using the following technologies:</h3>
-        <h3 style={{ fontSize: '15px' }}>    - "Cookies"are data files that are placed on your device or computer and often include an anonymous unique identifier. For more information about cookies, and how to disable cookies,</h3>
-        <h3 style={{ fontSize: '15px' }}>                visit http://www.allaboutcookies.org.</h3>
-        <h3 style={{ fontSize: '15px' }}>    - "Log files" track actions occurring on the Site, and collect data including your IP address, browser type, Internet service provider, referring/exit pages, and date/time stamps.</h3>
-        <h3 style={{ fontSize: '15px' }}>    - "Web beacons","tags","pixels are electronic files used to record information about how you browse the Site.</h3>
-        <h3 style={{ fontSize: '15px' }}>Additionally when you make a purchase or attempt to make a purchase through the Site, we collect certain information from you, including your name, billing address, shipping address, payment information (including credit card numbers), email address, and phone number.  We refer to this information as "Order Information."</h3>
-        <h3 style={{ fontSize: '15px' }}>When we talk about "Personal Information" in this Privacy Policy, we are talking both about Device Information and Order Information.</h3>
-        <h3 style={{ fontSize: '15px' }}>HOW DO WE USE YOUR PERSONAL INFORMATION?</h3>
-        <h3 style={{ fontSize: '15px' }}>We use the Order Information that we collect generally to fulfill any orders placed through the Site (including processing your payment information, arranging for delivery, and providing you with invoices and/or order confirmations).  Additionally, we use this Order Information to:</h3>
-        <h3 style={{ fontSize: '15px' }}>Communicate with you in particular changes to store opening times and status of your order via email, phone or SMS ;</h3>
-        <h3 style={{ fontSize: '15px' }}>Screen our orders for potential risk or fraud; and</h3>
-        <h3 style={{ fontSize: '15px' }}>When in line with the preferences you have shared with us, provide you with information or advertising relating to our products or services.</h3>
-        <h3 style={{ fontSize: '15px' }}>We use the Device Information that we collect to help us screen for potential risk and fraud (in particular, your IP address), and more generally to improve and optimize our Site (for example, by generating analytics about how our customers browse and interact with the Site, and to assess the success of our marketing and advertising campaigns).</h3>
-        <h3 style={{ fontSize: '15px' }}>We use the Device Information that we collect to show your targeted advertising on our products,services or promotions.</h3>
+        <h3 style={{ fontSize: '15px' }}>DISCLAIMER FOR INTERACTIVE SERVICES</h3>
+
+        <h3 style={{ fontSize: '15px' }}>Pizza Uno Sunderland maintains the interactive portion(s) of their Web site as a service free of charge. By using any interactive services provided herein, you are agreeing to comply with and be bound by the terms, conditions and notices relating to its use.</h3>
+
+        <h3 style={{ fontSize: '15px' }}>1. As a condition of your use of this Web site and the interactive services contained therein, you represent and warrant to Pizza Uno Sunderland that you will not use this Web site for any purpose that is unlawful or prohibited by these terms, conditions, and notices.</h3>
+
+        <h3 style={{ fontSize: '15px' }}>2. Pizza Uno Sunderland reserves the right at all times to disclose any information deemed by Pizza Uno Sunderland necessary to satisfy any applicable law, regulation, legal process or governmental request, or to edit, refuse to post or to remove any information or materials, in whole or in part.</h3>
+
+        <h3 style={{ fontSize: '15px' }}>3. The information, products, and services included on this Web site may include inaccuracies or typographical errors. Changes are periodically added to the information herein. Pizza Uno Sunderland may make improvements and/or changes in this Web site at any time. Advice received via this Web site should not be relied upon for personal, legal or financial decisions and you should consult an appropriate professional for specific advice tailored to your situation.</h3>
+
+        <h3 style={{ fontSize: '15px' }}>4. Pizza Uno Sunderland makes no representations about the suitability, reliability, timeliness, and accuracy of the information, products, and services contained on this web site for any purpose. All such information, products, and services are provided "as is" without warranty of any kind.</h3>
+
+        <h3 style={{ fontSize: '15px' }}>5. Pizza Uno Sunderland hereby disclaims all warranties and conditions with regard to the information, products, and services contained on this web site, including all implied warranties and conditions of merchantability, fitness for a particular purpose, title and non-infringement.</h3>
+
+        <h3 style={{ fontSize: '15px' }}>6. In no event shall Pizza Uno Sunderland be liable for any direct, indirect, punitive, incidental, special, consequential damages or any damages whatsoever including, without limitation, damages for loss of use, data or profits, arising out of or in any way connected</h3>
+
+        <h3 style={{ fontSize: '15px' }}>- with the use or performance of this web site,</h3>
+
+        <h3 style={{ fontSize: '15px' }}>- with the delay or inability to use this web site,</h3>
+
+        <h3 style={{ fontSize: '15px' }}>- with the provision of or failure to provide services, or</h3>
+
+        <h3 style={{ fontSize: '15px' }}>- for any information, software, products, services and related graphics obtained through this web site, or otherwise arising out of the use of this web site, whether based on contract, tort, strict liability or otherwise, even if Pizza Uno Sunderland has been advised of the possibility of damages.</h3>
+
+        <h3 style={{ fontSize: '15px' }}>7. Due to the fact that certain jurisdictions do not permit or recognize an exclusion or limitation of liability for consequential or incidental damages, the above limitation may not apply to you. If you are dissatisfied with any portion of this web site, or with any of these terms of use, your sole and exclusive remedy is to discontinue using this web site.</h3>
+
+        <h3 style={{ fontSize: '15px' }}>8. Pizza Uno Sunderland reserves the right in its sole discretion to deny any user access to this Web site, any interactive service herein, or any portion of this Web site without notice, and the right to change the terms, conditions, and notices under which this Web site is offered.</h3>
+
+        <h3 style={{ fontSize: '15px' }}>9. This Agreement contains the entire agreement between the parties relating to the subject matter hereof and supersedes any and all prior agreements or understandings, written or oral, between the parties related to the subject matter hereof.  No modification of this Agreement shall be valid unless made in writing and signed by both of the parties hereto.</h3>
+
+        <h3 style={{ fontSize: '15px' }}>10.This Agreement shall be governed by and construed in accordance with the laws of United Kingdom.</h3>
+
+        <h3 style={{ fontSize: '15px' }}>Binding Effect.  This Agreement shall be binding upon all who use Pizza Uno Sunderland services, property, and other assets mentioned in this agreement with respect to this Web site and associated content, and it supersedes all prior or contemporaneous communications and proposals, whether electronic, oral or written with respect to this Web site. A printed version of this agreement and of any notice given in electronic form shall be admissible in judicial or administrative proceedings based upon or relating to this agreement to the same extent and subject to the same conditions as other business documents and records originally generated and maintained in printed form. Fictitious names of companies, products, people, characters and/or data mentioned herein are not intended to represent any real individual, company, product or event. Any rights not expressly granted herein are reserved.</h3>
 
 
     </div>
