3ddbfc4391e2c806a0b4d94c33761628b8abbd91
uzair-shakeel <uzairshakeel00000@gmail.com>
2024-05-18 00:24:39 +0500

    fixed

--- api/index.js
+++ api/index.js
@@ -75,9 +75,7 @@ app.use((err, req, res, next) => {
   }
 });
 
-app.use(express.json({ limit: '20mb' }));
-app.use(express.urlencoded({ limit: '20mb', extended: true }));
-
+app.use(express.json());
 // Set storage engine for multer
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
